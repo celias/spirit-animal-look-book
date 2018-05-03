b8400198af12f6eb60df273fc8788984e734ae39
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-05-03 14:10:43 -0500

    firebase uploading

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyATxewgixjgH21_ACfhc63S0HIDdl6-J1w",
+  authDomain: "spiritphoto-7ed88.firebaseapp.com",
+  databaseURL: "https://spiritphoto-7ed88.firebaseio.com",
+  projectId: "spiritphoto-7ed88",
+  storageBucket: "spiritphoto-7ed88.appspot.com",
+  messagingSenderId: "800673803586"
 };
 
 firebase.initializeApp(config);
